a43557c3f8b26f4e87170921bdb1e7f3c521fae6
zdenduk <zdenek.david@educanet.cz>
2017-03-19 22:47:59 +0100

    Finished board presentation

--- app/src/main/java/net/zdendukmonarezio/takuzu/presentation/MainActivity.java
+++ app/src/main/java/net/zdendukmonarezio/takuzu/presentation/MainActivity.java
@@ -2,14 +2,35 @@ package net.zdendukmonarezio.takuzu.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import net.zdendukmonarezio.takuzu.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import nucleus.view.NucleusActivity;
+
+public class MainActivity extends NucleusActivity<MainPresenter> implements MainView {
+
+    @BindView(R.id.game_board_layout)
+    GameBoardLayout gameBoardLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        gameBoardLayout.setOnMoveListener((x, y) -> getPresenter().onMoveMade(x, y));
     }
+
+    @Override
+    public void showGameBoard(GameBoard gameBoard) {
+        gameBoardLayout.setBoard(gameBoard);
+    }
+
+    @Override
+    public void warn(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
 }
---
+++ app/src/main/java/net/zdendukmonarezio/takuzu/presentation/MainView.java
@@ -0,0 +1,7 @@
+package net.zdendukmonarezio.takuzu.presentation;
+
+public interface MainView {
+    void showGameBoard(GameBoard gameBoard);
+
+    void warn(String message);
+}
