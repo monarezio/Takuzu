5565e305554f33d227270b0b45fd2d0584185395
monarezio <samuel@kodytek.cz>
2017-03-26 08:12:47 +0200

    Added a set method to List extension

---
+++ app/src/main/java/net/zdendukmonarezio/takuzu/domain/models/extensions/Run.kt
@@ -0,0 +1,5 @@
+package net.zdendukmonarezio.takuzu.domain.models.extensions
+
+/**
+ * Created by samuelkodytek on 26/03/2017.
+ */
---
+++ app/src/main/java/net/zdendukmonarezio/takuzu/domain/models/utils/FieldPickerUtil.kt
@@ -0,0 +1,7 @@
+package net.zdendukmonarezio.takuzu.domain.models.utils
+
+/**
+ * Created by samuelkodytek on 26/03/2017.
+ */
+object FieldPickerUtil {
+}
