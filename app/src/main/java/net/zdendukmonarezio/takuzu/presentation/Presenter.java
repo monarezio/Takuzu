60a59b14c72e02857f7c9fe25449f243196db047
zdenduk <zdenek.david@educanet.cz>
2017-04-02 22:27:07 +0200

    Code explanations

--- app/src/main/java/net/zdendukmonarezio/takuzu/presentation/Presenter.java
+++ app/src/main/java/net/zdendukmonarezio/takuzu/presentation/Presenter.java
@@ -4,6 +4,10 @@ import nucleus.presenter.RxPresenter;
 import rx.Observable;
 
 public class Presenter<E> extends RxPresenter<E> {
+    /*
+     *  fixes RxPresenter view is null bug
+     *  > call viewIfExists() instead of view()
+     */
     public Observable<E> viewIfExists() {
         return view().filter(view -> view != null);
     }
