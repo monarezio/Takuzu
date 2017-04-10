a7f81c58be236689a80841b12edee51e9a5ed796
zdenduk <zdenek.david@educanet.cz>
2017-04-10 08:22:08 +0200

    Notification hotfix

--- app/src/main/java/net/zdendukmonarezio/takuzu/presentation/game/GamePresenter.java
+++ app/src/main/java/net/zdendukmonarezio/takuzu/presentation/game/GamePresenter.java
@@ -71,6 +71,7 @@ public class GamePresenter extends Presenter<GameView> {
         viewIfExists().subscribe(view -> {
             view.showGameBoard(game.getGameBoard(), gameSize);
             view.updatePercentStatus(0);
+            view.setNotification("");
         });
     }
 
